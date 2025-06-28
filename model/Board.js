7b5e38d44cff570d712196576fe36112b28263d1
0d-fako <od.fakorede@gmail.com>
2025-06-28 17:19:26 +0100

    Task Management API

--- model/Board.js
+++ model/Board.js
@@ -11,7 +11,9 @@ const boardSchema = new mongoose.Schema({
         type : mongoose.Schema.Types.ObjectId,
         ref : "User",
         required: true
-    },
+    }},
+    {timestamps : true});
 
-}
+    module.exports = mongoose.model("Board", boardSchema);
+
 
