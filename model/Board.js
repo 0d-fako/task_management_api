19e8cd3466ae87cbdb3a3c14791a785c5f01c462
0d-fako <od.fakorede@gmail.com>
2025-06-28 17:13:34 +0100

    Task Management API

---
+++ model/Board.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+
+const boardSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    user : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required: true
+    },
+
+}
+
